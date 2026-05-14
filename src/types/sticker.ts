6c9ffa819cb33ff4868fae4e68621db71d5d051c
Vahe <vahikpoghosyan@gmail.com>
2026-05-14 12:49:09 +0400

    chore: add prettier and reformat (#7)

--- src/types/sticker.ts
+++ src/types/sticker.ts
@@ -1,14 +1,17 @@
 export type Sticker = {
-  id: string;
-  x: number;
-  y: number;
-  title: string;
-  content: string;
-  zIndex: number;
+    id: string;
+    x: number;
+    y: number;
+    title: string;
+    content: string;
+    zIndex: number;
 };
 
 export type StickerProps = Sticker & {
-  onActivate: (id: string) => () => void;
-  onUpdate: (id: string, field: "title" | "content") => (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
-  onRemove: (id: string) => () => void;
+    onActivate: (id: string) => () => void;
+    onUpdate: (
+        id: string,
+        field: "title" | "content"
+    ) => (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+    onRemove: (id: string) => () => void;
 };
